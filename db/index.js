62b6f62969f71f8dab027e0bafa238971b0467a8
Jasonbuller94 <jason_buller94@yahoo.com>
2023-05-22 09:11:05 -0500

    Added ternary operator that uses the data base link when not in dev mode instead of just local database

--- db/index.js
+++ db/index.js
@@ -1,10 +1,14 @@
 const { Client } = require("pg"); // imports the pg module
 
-const client = new Client({
-  user: "postgres",
-  password: "postgres",
-  database: "juicebox-dev",
-});
+const client = new Client(
+  process.env.NODE_ENV === "dev"
+    ? {
+        user: "postgres",
+        password: "postgres",
+        database: "juicebox-dev",
+      }
+    : process.env.DB_URL
+);
 
 /**
  * USER Methods
